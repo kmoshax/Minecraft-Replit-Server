de23fc65d5ed14a4cdc2f3cae49dc9e68d5c418c
kmoshax <128246041+kmoshax@users.noreply.github.com>
2023-07-02 18:51:23 +0300

    -

---
+++ installer.sh
@@ -0,0 +1,43 @@
+echo -e "\e[1;34m==================================\e[0m"
+echo " ___  ___   __  __  ____  _  ____  
+| __||   \ |  \/  ||_  / / ||__  | 
+| _| | |) || |\/| | / /  | |  / /  
+|_|  |___/ |_|  |_|/___| |_| /_/  
+"
+echo -e "\e[1;34m| Starting...
+| Main author for this project is FDMZ17 & Edited By KmoSha.
+| Create backup.me file to backup server file.
+| For better performance use Hacker Plan Repl.
+| Supported version: 1.20.x\e[0m".
+echo -e "\e[1;34m==================================\e[0m"
+read -r -p "
+Could find server.sh
+1) PaperMC    [JAVA]
+2) Purpur     [JAVA]
+3) Vanila     [JAVA]
+4) Exit
+Choose a Number from 1 to 4.
+> " response
+if [[ "$response" =~ ^([1][eE][sS]|[1])$ ]]
+then
+    wget https://raw.githubusercontent.com/FDMZ17/replit-minecraft-server-file/main/paper/server.sh
+    bash server.sh
+fi    
+
+if [[ "$response" =~ ^([2][eE][sS]|[2])$ ]]
+then
+    wget https://raw.githubusercontent.com/FDMZ17/replit-minecraft-server-file/main/purpur/server.sh
+    bash server.sh
+fi
+
+if [[ "$response" =~ ^([3][eE][sS]|[3])$ ]]
+then
+    wget https://raw.githubusercontent.com/FDMZ17/replit-minecraft-server-file/raw/main/vanila/server.sh
+    bash server.sh
+fi
+
+
+if [[ "$response" =~ ^([4][eE][sS]|[4])$ ]]
+then
+    exit
+fi
---
+++ main.sh
@@ -0,0 +1,15 @@
+FILE=backup.me
+if [ -f $FILE ]; then
+   echo -e "\e[1;33mkmosha@script~\e[0m" Starting backup script...
+   bash backup.sh
+   rm backup.me
+else
+   echo 
+fi
+
+FILE=server.sh
+if [ -f $FILE ]; then
+   bash server.sh
+else
+   bash installer.sh
+fi
